40370bcb4f0a5d4257b99ee3ae8a8d881d7ed7f5
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-11-26 10:45:03 -0500

    day 4 part 1

---
+++ 2020/day_4/day_4_part_2.rb
@@ -0,0 +1,39 @@
+file='2020/day_4/input.txt'
+valid_passports = []
+current_passport = {}
+
+def check_if_valid_passport(passport)
+  if passport.size < 7
+    false
+  elsif passport.size == 7 && passport.key?("cid")
+    false
+  else
+    check_values_of_passport(passport)
+  end
+end
+
+def check_values_of_passport(passport)
+  passport.each
+end
+
+f = File.open(file, "r")
+f.each_line do |line|
+  if line.strip.empty?
+    if check_if_valid_passport(current_passport)
+      valid_passports << current_passport
+    end
+    current_passport = {}
+  end
+
+  arr = line.strip.split(' ')
+  arr.map do |v|
+    k_v = v.split(':')
+    current_passport[k_v[0]] = k_v[1]
+  end
+end
+
+if check_if_valid_passport(current_passport)
+  valid_passports << current_passport
+end
+
+puts valid_passports.count
