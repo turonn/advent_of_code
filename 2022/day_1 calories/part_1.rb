327e44552129687617c8e02b2e10b367b9d222cf
turonn <72614966+turonn@users.noreply.github.com>
2022-12-01 00:12:52 -0500

    2022 day 1 elf calories

---
+++ 2022/day_1 calories/part_1.rb
@@ -0,0 +1,19 @@
+file='2022/day_1/input.txt'
+elves = []
+single_elf = []
+
+f = File.open(file, "r")
+f.each_line do |line|
+  cals = line.strip.to_i
+  if cals == 0
+    elves << single_elf
+    single_elf = []
+    next
+  end
+
+  single_elf << cals
+end
+
+elves << single_elf
+
+puts elves.map(&:sum).max
