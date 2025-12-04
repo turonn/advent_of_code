7d932bba837c21ccfe078c15c66a94ebfd96d6cf
Nicholas Turon <nicholas.turon@joinroot.com>
2025-12-04 00:43:28 -0500

    2025 day 4

---
+++ 2025/day_4_forklift/part_1.rb
@@ -0,0 +1,88 @@
+class Space
+  attr_reader :x, :y, :value
+  attr_accessor :adjacent_rolls, :neighbors
+  def initialize(x, y, value)
+    @x = x
+    @y = y
+    @value = value
+    @has_roll = value == '@'
+    @neighbors = []
+    @adjacent_rolls = 0
+    @satisfies_condition = false
+  end
+
+  def count_neighbors
+    @adjacent_rolls = @neighbors.count(&:has_roll?)
+    @satisfies_condition = @adjacent_rolls < 4
+  end
+
+  def has_roll?
+    @has_roll
+  end
+
+  def satisfies_condition?
+    @satisfies_condition
+  end
+end
+
+class Map
+  attr_reader :accessible_rolls
+
+  # @param rows [Array<Array<Space>>]
+  # @param row [Array<Space>]
+  def initialize(rows)
+    @rows = rows
+    @width_index = rows.first.length - 1
+    @height_index = rows.length - 1
+    @accessible_rolls = 0
+  end
+
+  def add_neighbors_and_count
+    @rows.each_with_index do |row, y|
+      row.each_with_index do |space, x|
+        # we don't need to count the neighbors if the space doesn't have a roll
+        next unless space.has_roll?
+
+        if y > 0
+          # up
+          space.neighbors << @rows[y-1][x]
+          # up left
+          space.neighbors << @rows[y-1][x-1] if x > 0
+          # up right
+          space.neighbors << @rows[y-1][x+1] if x < @width_index
+        end
+
+        if y < @height_index
+          # down
+          space.neighbors << @rows[y+1][x]
+          # down left
+          space.neighbors << @rows[y+1][x-1] if x > 0
+          # down right
+          space.neighbors << @rows[y+1][x+1] if x < @width_index
+        end
+
+        # left
+        space.neighbors << @rows[y][x-1] if x > 0
+        # right
+        space.neighbors << @rows[y][x+1] if x < @width_index
+
+        space.count_neighbors
+
+        @accessible_rolls += 1 if space.satisfies_condition?
+      end
+    end
+  end
+end
+
+rows = []
+File.open('2025/day_4_forklift/input.txt', "r").each_line.with_index do |line, y|
+  row = line.strip.split('').each_with_index.map do |value, x|
+    Space.new(x, y, value)
+  end
+  rows << row
+end
+
+map = Map.new(rows)
+
+map.add_neighbors_and_count
+puts map.accessible_rolls
